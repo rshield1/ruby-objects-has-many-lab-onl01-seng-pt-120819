7df4a971052330dbcf9ed1180f3668870d454e77
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-05 22:46:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,17 +15,17 @@ class Artist
    def add_song(song)
     song.artist = self
   end 
-  
+    def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
   def self.all
     @@all
   end
 
 
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
 
 
 
