357f8368d6fc5147e2f31922e878fe293605b898
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-05 22:45:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,13 +12,15 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
   
+   def add_song(song)
+    song.artist = self
+  end 
+  
   def self.all
     @@all
   end
 
-  def add_song(song)
-    song.artist = self
-  end
+
 
   def add_song_by_name(name)
     song = Song.new(name)
