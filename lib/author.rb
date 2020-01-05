1c876ef48ff1410578b725978ffb66a2024a7b39
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-05 22:56:21 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,10 +15,12 @@ class Author
   def add_post(post)
     post.author = self
   end
-    def add_post_by_title(title)
+  
+  def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
   end
+  
   def self.all
     @@all
   end
