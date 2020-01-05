46a788bc0a0bc68d3b2ce3625f5761b516c845bb
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-05 22:57:10 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,24 +7,24 @@ class Artist
     @name = name
     @@all << self
   end
-
-  def self.all
-    @@all
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
-
-  def add_song(song)
+  
+   def add_song(song)
     song.artist = self
-  end
-
+  end 
+  
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def self.all
+    @@all
   end
-
+  
   def self.song_count
     Song.all.count
   end
